63b470019e5baeadb7b59d5c4a656b01713adcce
webdev-ashishk <webdev.ashishk@gmai.com>
2023-03-08 07:31:09 -0500

    extra-lines and spaces has been deleted

--- src/script.js
+++ src/script.js
@@ -3,13 +3,10 @@
         let h=d.getHours();
         let m=d.getMinutes();
         let s=d.getSeconds();
-        
         return ` ${h} : ${m} : ${s}`;
-
       };
       console.log(getTime());
       setInterval(()=>{
-        // console.log(getTime());
      let clockObj=   document.querySelector(".clock")
           clockObj.innerHTML=getTime();
       },1000)
